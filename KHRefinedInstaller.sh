e335b903608c3d1ee30fcc7c6e31686b3a3a697d
Kurumi78 <30587387+Kurumi78@users.noreply.github.com>
2023-05-28 19:02:20 -0500

    Update KHRefinedInstaller.sh

--- KHRefinedInstaller.sh
+++ KHRefinedInstaller.sh
@@ -27,43 +27,54 @@ mkdir ~/RefinedSetup
 
 cd ~/RefinedSetup
 
-echo "Downloading Kingdom Hearts Re:Fined 3.00 by TopazTK"
-curl -LJO https://github.com/TopazTK/KH-ReFined/releases/download/v3.00/ReFined_v3.00.zip
-unzip ReFined_v3.00.zip
+echo "Downloading Kingdom Hearts Re:Fined 4.00 by TopazTK"
+#curl -LJO https://github.com/TopazTK/KH-ReFined/releases/download/v3.00/ReFined_v3.00.zip
+#unzip ReFined_v3.00.zip
 
+curl -LJO KH2EXEDownloadHere
+curl -LJO https://cloud.audreyvps.net/s/KHPCPatchManager/download/KHPCPatchManager.exe
+curl -LJO PCpatchHere
 
+kh2DAStatus = false;
+khBBSDAstatus = false;
 mkdir "${khInstall}/backup"
-while true; do
+mv "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS II FINAL MIX.exe"
+mv "KH2.exe" "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe"
 
-read -p "Which version of Re:Fined would you like to download? To select which game, please enter one game at a time (kh1/kh2/kh2da/bbs/bbsda). When you are finished, enter ''done'' without the quotation marks and hit enter. " GameSel
+#while true; do
 
-case $GameSel in
-        [Kk][Hh]1 | [Kk][Hh][Ii] | [Kk][Hh] )
-               mv "${khInstall}/KINGDOM HEARTS FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS FINAL MIX.exe"
-               mv "Kingdom Hearts I/Kingdom Hearts I.exe" "${khInstall}/KINGDOM HEARTS FINAL MIX.exe"
-               ;;
-        [Kk][Hh]2 | [Kk][Hh][Ii][Ii] ) 
-               mv "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS II FINAL MIX.exe"
-               mv "Kingdom Hearts II/Kingdom Hearts II.exe" "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe" 
-               ;;
-        [Kk][Hh]2[Dd][Aa] | [Kk][Hh][Ii][Ii][Dd][Aa] )
-               mv "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS II FINAL MIX.exe"
-               mv "Kingdom Hearts II/Kingdom Hearts II - Dual Audio.exe" "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe"
-               ;;
-        [Bb][Bb][Ss] )
-               mv "${khInstall}/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe"
-               mv "Birth by Sleep/Birth by Sleep.exe" "${khInstall}/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe"
-               ;;
-        [Bb][Bb][Ss][Dd][Aa] )
-               mv "${khInstall}/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe"
-               mv "Birth by Sleep/Birth by Sleep - Dual Audio.exe" "${khInstall}/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe"
-               ;;
-	    [Dd][Oo][Nn][Ee] ) break;;
-   
-        * ) echo invalid response;;
-esac
+#read -p "Which version of Re:Fined would you like to download? To select which game, please enter one game at a time (kh1/kh2/kh2da/bbs/bbsda). When you are finished, enter ''done'' without the quotation marks and hit enter. " GameSel
 
-done
+
+#case $GameSel in
+#        [Kk][Hh]1 | [Kk][Hh][Ii] | [Kk][Hh] )
+#               mv "${khInstall}/KINGDOM HEARTS FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS FINAL MIX.exe"
+#               mv "Kingdom Hearts I/Kingdom Hearts I.exe" "${khInstall}/KINGDOM HEARTS FINAL MIX.exe"
+#               ;;
+#        [Kk][Hh]2 | [Kk][Hh][Ii][Ii] ) 
+#               mv "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS II FINAL MIX.exe"
+#               mv "Kingdom Hearts II/Kingdom Hearts II.exe" "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe" 
+#               ;;
+#        [Kk][Hh]2[Dd][Aa] | [Kk][Hh][Ii][Ii][Dd][Aa] )
+#               mv "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS II FINAL MIX.exe"
+#               mv "Kingdom Hearts II/Kingdom Hearts II - Dual Audio.exe" "${khInstall}/KINGDOM HEARTS II FINAL MIX.exe"
+#				kh2DAStatus = true;
+#               ;;
+#        [Bb][Bb][Ss] )
+#               mv "${khInstall}/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe"
+#               mv "Birth by Sleep/Birth by Sleep.exe" "${khInstall}/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe"
+#               ;;
+#        [Bb][Bb][Ss][Dd][Aa] )
+#               mv "${khInstall}/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe" "${khInstall}/backup/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe"
+#               mv "Birth by Sleep/Birth by Sleep - Dual Audio.exe" "${khInstall}/KINGDOM HEARTS Birth by Sleep FINAL MIX.exe"
+#				khBBSDAstatus = true;
+#               ;;
+#	    [Dd][Oo][Nn][Ee] ) break;;
+#   
+#        * ) echo invalid response;;
+#esac
+
+#done
 
 read -p "Would you like to install the Offline Patch? This will allow you to play the games offline and bypass the game launcher, but save data will be made with the EGS ID of ''1638'', which may make older save files not made with this ID incompatable! (y/n) " OCon
 
@@ -89,9 +100,14 @@ mv "OfflinePatch/epic_emu.ini" "${khInstall}"
 
 fi
 
-echo "Deleting Temporary Working Directories"
 
-rm -rf ~/RefinedSetup/
+
+
+
+
+
+
+
 
 
 echo "Creating new Wineprefix"
@@ -99,21 +115,29 @@ echo "Installing dependencies to Wineprefix, this may take awhile."
 
 while true; do
 
-read -p "Will you be running Kingdom Hearts with Proton/Proton-GE, or Wine/Wine-GE? (proton/wine) " ProCon
+read -p "Will you be running Kingdom Hearts with Proton/Proton-GE, or Wine/Wine-GE? (proton/wine/skip) " ProCon
 
-#echo "$prefixlocation" 
+echo "$prefixlocation" 
  
 case $ProCon in
         [Pp][Rr][Oo][Tt][Oo][Nn] ) WINEPREFIX=~/RefinedPrefix/pfx winetricks -q -f dotnet48 vcrun2022 win10
-	       break;;
-        [Ww][Ii][Nn][Ee] ) WINEPREFIX=~/RefinedPrefix winetricks -q -f dotnet48 vcrun2022 win10
+               break;;
+       [Ww][Ii][Nn][Ee] ) WINEPREFIX=~/RefinedPrefix winetricks -q -f dotnet48 vcrun2022 win10
       	       break;;
+       [Ss][Kk][Ii][Pp] ) break;;
         * ) echo invalid response;;
 esac
 
 done
+WINEPREFIX=~/RefinedPrefix wine KHPCPatchManager.exe ReFinedBETA-SteamOS.kh2pcpatch "${khInstall}/Image/en"
+#WINEPREFIX=~/RefinedPrefix wine KHPCPatchManager.exe KH2DAPatch.kh2pcpatch "${khInstall}/Image/en"
+#WINEPREFIX=~/RefinedPrefix wine KHPCPatchManager.exe BBSDAPatch.BBSpcpatch "${khInstall}/Image/en"
+echo "Deleting Temporary Working Directories"
+rm -rf ~/RefinedSetup/
+
 echo "Renaming EPIC to EPIC.bak to prevent FMV videos from crashing the game..."
 mv "${khInstall}/EPIC" "${khInstall}/EPIC.bak"
 
 echo "Thanks for using my Re:Fined Installer. If you run into issues, contact Kurumi Tokisaki#0001 on Discord."
+
 
